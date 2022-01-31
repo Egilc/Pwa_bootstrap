52164a161f2b88ba0644c8c466c1c5b5d729f7b5
Egil <egc21@hotmail.com>
2022-01-31 11:40:41 -0500

    commit on 'Laboratoire2_appProgressive'

--- sw.js
+++ sw.js
@@ -5,6 +5,7 @@ const APP_SHELL = [
    "index.html",
    "House.jpeg",
    "Conseiller.jpeg",
+   "feliz.jpeg",
    "bootstrap-5.1.3-dist/css/bootstrap.min.css",
   "icons-1.7.2/font/bootstrap-icons.css",
   "bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js",
